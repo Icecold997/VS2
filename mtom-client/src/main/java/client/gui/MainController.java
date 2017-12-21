9d161d7280ec69ee946b33f9d5fb64e284a45936
Sander Timo <timo_sander@gmx.de>
2017-12-21 19:47:48 +0100

    erweitert gui

--- mtom-client/src/main/java/client/gui/MainController.java
+++ mtom-client/src/main/java/client/gui/MainController.java
@@ -4,14 +4,14 @@ import client.ws.DocumentsClient;
 import de.htwsaar.FileView;
 import de.htwsaar.Document;
 
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 import org.apache.derby.impl.tools.sysinfo.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -74,8 +74,52 @@ public class MainController implements Initializable {
        table_type.setCellValueFactory(new PropertyValueFactory<FileView, String>("type"));
 
        table_view.setItems(fileViewList.getFileViewList());
+       table_view.setRowFactory(
+               new Callback<TableView<FileView>, TableRow<FileView>>() {
+                   @Override
+                   public TableRow<FileView> call(final TableView<FileView> tableView) {
+                       final TableRow<FileView> row = new TableRow<>();
+                       final ContextMenu rowMenu = new ContextMenu();
+                       MenuItem editItem = new MenuItem("Umbennen");
+                       editItem.setOnAction(new EventHandler<ActionEvent>() {
+
+                           @Override
+                           public void handle(ActionEvent event) {
+                               renameDocument(row.getItem());
+                           }
+                       });
+                       MenuItem removeItem = new MenuItem("Löschen");
+                       removeItem.setOnAction(new EventHandler<ActionEvent>() {
+
+                           @Override
+                           public void handle(ActionEvent event) {
+                               deleteDocument(row.getItem());
+                           }
+                       });
+                       rowMenu.getItems().addAll(editItem, removeItem);
+
+// only display context menu for non-null items:
+
+                       row.contextMenuProperty().bind(
+                               Bindings.when(Bindings.isNotNull(row.itemProperty()) )
+                                       .then(rowMenu)
+                                       .otherwise((ContextMenu)null));
+                       return row;
+                   }
+
+               });
+    }
+    private void renameDocument(FileView fileView){
+        //documentsClient.renameDocument()
+        System.out.println("testclick");
     }
 
+    private void deleteDocument(FileView fileView){
+        if( documentsClient.deleteDocument(fileView.getFileOrDirectoryName())) {
+            table_view.getItems().remove(fileView);
+        }
+    }
+    
     @FXML
     private void chanceDownloadDirectory(){
         downloadDirectoryLabelDynamicText.setText(router.startDirectoryChooser());
